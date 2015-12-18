a6ac9e7dd0bae28746d700eaa3a55ca61a733353
Nicholas Whyte <nwhyte@atlassian.com>
2015-12-18 03:52:35 +0100

    Add Shell completion Support
    (cherry picked from commit 2ca6c6ae68542fcd6c07270c4a2373c4cdc063c8)

--- parser.go
+++ parser.go
@@ -97,6 +97,7 @@ type ParseContext struct {
 	peek            []*Token
 	argi            int // Index of current command-line arg we're processing.
 	args            []string
+	rawArgs         []string
 	flags           *flagGroup
 	arguments       *argGroup
 	argumenti       int // Cursor into arguments
@@ -130,6 +131,7 @@ func tokenize(args []string, ignoreDefault bool) *ParseContext {
 	return &ParseContext{
 		ignoreDefault: ignoreDefault,
 		args:          args,
+		rawArgs:       args,
 		flags:         newFlagGroup(),
 		arguments:     newArgGroup(),
 	}
