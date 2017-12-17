e25af95dcef18326569a46b2b561f13107f6548c
Alec Thomas <alec@swapoff.org>
2017-12-17 10:08:21 -0800

    Fix error propagation from @ failures.
    Really fixes #185.
    
    (cherry picked from commit 947dcec5ba9c011838740e680966fd7087a71d0d)

--- parser.go
+++ parser.go
@@ -158,6 +158,10 @@ func (p *ParseContext) EOL() bool {
 	return p.Peek().Type == TokenEOL
 }
 
+func (p *ParseContext) Error() bool {
+	return p.Peek().Type == TokenError
+}
+
 // Next token in the parse context.
 func (p *ParseContext) Next() *Token {
 	if len(p.peek) > 0 {
@@ -271,9 +275,12 @@ func (p *ParseContext) matchedCmd(cmd *CmdClause) {
 
 // Expand arguments from a file. Lines starting with # will be treated as comments.
 func ExpandArgsFromFile(filename string) (out []string, err error) {
+	if filename == "" {
+		return nil, fmt.Errorf("expected @ file to expand arguments from")
+	}
 	r, err := os.Open(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open arguments file %q: %s", filename, err)
 	}
 	defer r.Close()
 	scanner := bufio.NewScanner(r)
@@ -285,6 +292,9 @@ func ExpandArgsFromFile(filename string) (out []string, err error) {
 		out = append(out, line)
 	}
 	err = scanner.Err()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read arguments from %q: %s", filename, err)
+	}
 	return
 }
 
@@ -296,7 +306,7 @@ func parse(context *ParseContext, app *Application) (err error) {
 	ignoreDefault := context.ignoreDefault
 
 loop:
-	for !context.EOL() {
+	for !context.EOL() && !context.Error() {
 		token := context.Peek()
 
 		switch token.Type {
@@ -373,6 +383,10 @@ loop:
 		} else {
 			break
 		}
+	}
+
+	if context.Error() {
+		return fmt.Errorf("%s", context.Peek().Value)
 	}
 
 	if !context.EOL() {
