955002ddfbb68d07b9e0922cc8f7fc40591aa76f
Arjun mahishi <arjun.mahishi@gmail.com>
2023-04-07 14:03:34 +0530

    Handle --version just like --help
    when the version flag is passed, the application should do nothing but
    print the version and exit. But currently, the pre-action for the
    version flag got executed AFTER the default for all the other flags were
    set.
    
    Setting defaults involves validation like checking for file's
    existence etc. These validation are not necessary when the --version
    flag is passed. This commit fixes that
    
    (cherry picked from commit b4b1df31f0defc03e8e20b51973dbcb4a6c462b1)

--- app.go
+++ app.go
@@ -425,6 +425,10 @@ func (a *Application) setDefaults(context *ParseContext) error {
 			if flag.name == "help" {
 				return nil
 			}
+
+			if flag.name == "version" {
+				return nil
+			}
 			flagElements[flag.name] = element
 		}
 	}
